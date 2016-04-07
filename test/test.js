8fa3c8c076be1fa8e5a46407ff6cc3188a137fd5
nathanchapman <nathanchapman@utexas.edu>
2016-04-07 14:42:09 -0500

    Add mocha tests and travis CI

---
+++ test/test.js
@@ -0,0 +1,50 @@
+var chai = require('chai'),
+assert = chai.assert,
+expect = chai.expect,
+should = chai.should(),
+busted = require('../busted.js');
+
+describe('removeProtocolFromURL', function() {
+  describe('remove protocol', function () {
+    it('should remove http:// from the URL', function (done) {
+      var URL = "http://facebook.com";
+      expect(busted.removeProtocolFromURL(URL)).to.equal('facebook.com');
+      done();
+    });
+    it('should remove https:// from the URL', function (done) {
+      var URL = "https://facebook.com";
+      expect(busted.removeProtocolFromURL(URL)).to.equal('facebook.com');
+      done();
+    });
+    it('should remove www. from the URL', function (done) {
+      var URL = "www.facebook.com";
+      expect(busted.removeProtocolFromURL(URL)).to.equal('facebook.com');
+      done();
+    });
+    it('should remove http(s):// and www. from the URL', function (done) {
+      var URL = "http://www.facebook.com";
+      expect(busted.removeProtocolFromURL(URL)).to.equal('facebook.com');
+      done();
+    });
+  });
+});
+
+describe('standardizeURL', function() {
+  describe('standardize URL with protocol if missing', function () {
+    it('should do nothing if http:// is present', function (done) {
+      var URL = "http://www.facebook.com";
+      expect(busted.standardizeURL(URL)).to.equal('http://www.facebook.com');
+      done();
+    });
+    it('should do nothing if https:// is present', function (done) {
+      var URL = "https://www.facebook.com";
+      expect(busted.standardizeURL(URL)).to.equal('https://www.facebook.com');
+      done();
+    });
+    it('should add http:// if protocol is missing', function (done) {
+      var URL = "facebook.com";
+      expect(busted.standardizeURL(URL)).to.equal('http://facebook.com');
+      done();
+    });
+  });
+});
