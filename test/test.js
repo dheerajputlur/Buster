0e508d1b7a15a10a73c8f33736c57fffd6a9998a
nathanchapman <nathanchapman@utexas.edu>
2016-04-07 19:56:57 -0500

    Add iframe tests

--- test/test.js
+++ test/test.js
@@ -1,27 +1,27 @@
 var chai = require('chai'),
-assert = chai.assert,
-expect = chai.expect,
-should = chai.should(),
-busted = require('../busted.js');
+  assert = chai.assert,
+  expect = chai.expect,
+  should = chai.should(),
+  busted = require('../busted.js');
 
 describe('removeProtocolFromURL', function() {
-  describe('removes protocol from a given URL', function () {
-    it('should remove http:// from the URL', function (done) {
+  describe('removes protocol from a given URL', function() {
+    it('should remove http:// from the URL', function(done) {
       var URL = "http://facebook.com";
       expect(busted.removeProtocolFromURL(URL)).to.equal('facebook.com');
       done();
     });
-    it('should remove https:// from the URL', function (done) {
+    it('should remove https:// from the URL', function(done) {
       var URL = "https://facebook.com";
       expect(busted.removeProtocolFromURL(URL)).to.equal('facebook.com');
       done();
     });
-    it('should remove www. from the URL', function (done) {
+    it('should remove www. from the URL', function(done) {
       var URL = "www.facebook.com";
       expect(busted.removeProtocolFromURL(URL)).to.equal('facebook.com');
       done();
     });
-    it('should remove http(s):// and www. from the URL', function (done) {
+    it('should remove http(s):// and www. from the URL', function(done) {
       var URL = "http://www.facebook.com";
       expect(busted.removeProtocolFromURL(URL)).to.equal('facebook.com');
       done();
@@ -30,18 +30,18 @@ describe('removeProtocolFromURL', function() {
 });
 
 describe('standardizeURL', function() {
-  describe('standardizes URL with http protocol if missing', function () {
-    it('should do nothing if http:// is present', function (done) {
+  describe('standardizes URL with http protocol if missing', function() {
+    it('should do nothing if http:// is present', function(done) {
       var URL = "http://www.facebook.com";
       expect(busted.standardizeURL(URL)).to.equal('http://www.facebook.com');
       done();
     });
-    it('should do nothing if https:// is present', function (done) {
+    it('should do nothing if https:// is present', function(done) {
       var URL = "https://www.facebook.com";
       expect(busted.standardizeURL(URL)).to.equal('https://www.facebook.com');
       done();
     });
-    it('should add http:// if protocol is missing', function (done) {
+    it('should add http:// if protocol is missing', function(done) {
       var URL = "facebook.com";
       expect(busted.standardizeURL(URL)).to.equal('http://facebook.com');
       done();
@@ -49,22 +49,29 @@ describe('standardizeURL', function() {
   });
 });
 
-// describe('iframeTest', function() {
-//   describe('checks if iframe\'s location is equal to a given URL', function () {
-//     it('should do nothing if http:// is present', function (done) {
-//       var URL = "http://www.facebook.com";
-//       expect(busted.standardizeURL(URL)).to.equal('http://www.facebook.com');
-//       done();
-//     });
-//     it('should do nothing if https:// is present', function (done) {
-//       var URL = "https://www.facebook.com";
-//       expect(busted.standardizeURL(URL)).to.equal('https://www.facebook.com');
-//       done();
-//     });
-//     it('should add http:// if protocol is missing', function (done) {
-//       var URL = "facebook.com";
-//       expect(busted.standardizeURL(URL)).to.equal('http://facebook.com');
-//       done();
-//     });
-//   });
-// });
+describe('iframeTest', function() {
+  describe('checks if iframe\'s location is equal to a given URL', function() {
+    var iframe = {
+      contentWindow: {
+        location: {
+          href: "http://facebook.com"
+        }
+      }
+    };
+    it('should return false if the URL was found', function(done) {
+      var URL = "http://facebook.com";
+      expect(busted.iframeTest(URL, iframe)).to.equal(false);
+      done();
+    });
+    it('should return false if similar host URL is found', function(done) {
+      var URL = "https://www.facebook.com";
+      expect(busted.iframeTest(URL, iframe)).to.equal(false);
+      done();
+    });
+    it('should return true if similar host URL is not found', function(done) {
+      var URL = "https://www.google.com";
+      expect(busted.iframeTest(URL, iframe)).to.equal(true);
+      done();
+    });
+  });
+});
