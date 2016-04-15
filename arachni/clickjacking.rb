367dffc408f291faf897496d317a779995bf97df
nathanchapman <nathanchapman@utexas.edu>
2016-04-15 14:47:06 -0500

    Update arachni regex for css matching

--- arachni/clickjacking.rb
+++ arachni/clickjacking.rb
@@ -1,21 +1,21 @@
 =begin
-    
+
 =end
 
 # Checks to see if site is vunerable to clickjacking.
 #
 # @author Kyle Schmid <kyleschmid@me.com>
 class Arachni::Checks::Clickjacking < Arachni::Check::Base
-    
+
     def run
-        
+
         print_status 'Running Clickjacking Check'
-        
+
         http.request( page.url, method: :get) { |response| check_and_log( response ) }
     end
-    
+
     def check_and_log( response )
-        
+
         # Check for X-Frame-Options and Content-Security-Policy/X-Content-Security-Policy. Pass if enabled. Continue checks otherwise.
         print_info 'Checking if page has X-Frame-Options enabled'
         if (response.headers.key?('X-Frame-Options') && (response.headers['X-Frame-Options'] == 'DENY' || response.headers['X-Frame-Options'] == 'SAMEORIGIN'))
@@ -23,7 +23,7 @@ class Arachni::Checks::Clickjacking < Arachni::Check::Base
 		elsif (response.headers.key?('Content-Security-Policy') && response.headers['Content-Security-Policy'] =~ /frame-ancestors/ && response.headers.key?('X-Content-Security-Policy') && response.headers['X-Content-Security-Policy'] =~ /frame-ancestors/)
 			print_info 'Page implements Content-Security-Policy and X-Content-Security-Policy, so it is not vunerable to clickjacking.'
         else
-            
+
             body = response.to_page.body.gsub(/<!--(.*?)-->/m, "").downcase # remove comments and downcase
             open_scripts = body.split(/<script.*?>/m) # split by script tag
             open_scripts.delete_at(0) # remove whatever is before first script tag
@@ -31,7 +31,7 @@ class Arachni::Checks::Clickjacking < Arachni::Check::Base
             scripts = []
             open_scripts.each { |a| scripts << a.gsub(/<\/script>.*/m, "") }
             scripts.each { |a| print_info a }
-            
+
             # Check for Double Framing vuneralbility
             print_info 'Checking if page is vunerable to double framing'
             # Check to see if each script tries to access parent.location. If so, it's vunerable to double framing.
@@ -41,7 +41,7 @@ class Arachni::Checks::Clickjacking < Arachni::Check::Base
                     return
                 end
             }
-            
+
             # Check for referrer checking vunerability
             print_info 'Checking if page is vunerable to an attack exploiting referrer checking'
             # Check to see if each script attempts to look at document.referrer.indexof
@@ -53,12 +53,12 @@ class Arachni::Checks::Clickjacking < Arachni::Check::Base
             }
             
             # Check to see if display is none at beginning. Otherwise, many clickjacking attacks will work.
-            if !(body =~ /<style.*?>.*?\shtml\s*{\s*display\s*:\s*none\s*;?\s*}.*<\/style>/m)
+            if !(body =~ /<style.*?>(.*?[\s*\n*;])?html[\s*\n*]?{(.*?[\s*\n*;])?display\s*:\s*none\s*[;\s*\n*]*}.*<\/style>/m)
                 log( vector: Element::Server.new( response.url ), response: response, proof: body )
             end
         end
     end
-    
+
     def self.info
         {
             name:        'Clickjacking',
@@ -67,11 +67,11 @@ Checks to see if a page is vunerable to clickjacking by checking the page for va
 },
             author:      'Kyle Schmid (kyleschmid@me.com)',
             version:     '0.1',
-            
+
             issue:       {
                 name:                   %q{Clickjacking issue},
                 description:            %q{
-                
+
                 This content is courtesy of Gustav Rydstedt,
                 Elie Bursztein, and Dan Boneh of Stanford
                 University as well as Collin Jackson of
@@ -80,13 +80,13 @@ Checks to see if a page is vunerable to clickjacking by checking the page for va
                 of Clickjacking Vulnerabilities on Popular Sites.
                 This publication can be found at
                 http://w2spconf.com/2010/papers/p27.pdf.
-                
+
                 Clickjacking occurs when an attacking website is
                 able to successfully frame a page and use that
                 frame in order to hijack a user's web session.
                 Many common clickjacking techniques include
                 various vunerabilities in the frame busting code:
-                
+
                 - Accessing parent.location: Vunerable to double
                   framing. Accessing parent.location becomes a
                   security violation in major browsers when page
@@ -114,18 +114,18 @@ Checks to see if a page is vunerable to clickjacking by checking the page for va
                         Zone and Sandbox or Design mode to disable
                         all javascript, including frame-busting
                         script.
-                
+
                 The recommended approach for successfully frame-
                 busting is to initially hide all content on the
                 page, then use javascript to either bust out of
                 the frame or display content. This way, if
                 javascript is disabled in some manner, no content
                 will be displayed. Here is sample code:
-                    
+
                     <style>
                       html { display:none; }
                     </style>
-                    
+
                     <script>
                       if (self == top) {
                         document.documentElement.style.display = 'block';
@@ -133,12 +133,12 @@ Checks to see if a page is vunerable to clickjacking by checking the page for va
                         top.location = self.location;
                       }
                     </script>
-                
+
                 In addition to this method, you could use the
                 X-Frame-Options header.
                 },
                 severity:               Severity::INFORMATIONAL
-            } 
+            }
         }
     end
 
